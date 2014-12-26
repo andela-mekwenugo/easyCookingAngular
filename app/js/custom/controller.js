0c48d5a5d8a5695dc0223387305444fa45cbd556
mirabelkoso <mirabelkoso@gmail.com>
2014-12-26 13:42:30 +0100

    more error handling

--- app/js/custom/controller.js
+++ app/js/custom/controller.js
@@ -5,17 +5,21 @@ var recipeController = recipeApp.controller;
 recipeApp.controller('recipeController',['$scope', '$http', function($scope,$http) {
 
   $scope.searchRecipe = function(recipeName) {
-      $scope.recipeName = recipeName;
-      $('.load').show();
-      $http.get("http://api.bigoven.com/recipes?title_kw=" + $scope.recipeName + "&pg=1&rpp=20&api_key=dvx0hom7nq400HGdFUJ164n6U7H9eA2V").
-      success(function(data) {
-        if(data.Results.length === 0) {
-            $('.loadArea').html("No result found. Please Enter Another Recipe Name");
-            $('.load').hide();
-        }
-        $scope.Results = data;
-        $scope.recipes = data.Results;
-        console.log(data);
-      });
+    $scope.recipeName = recipeName;
+    $('.load').show();
+    $http.get("http://api.bigoven.com/recipes?title_kw=" + $scope.recipeName + "&pg=1&rpp=20&api_key=dvx0hom7nq400HGdFUJ164n6U7H9eA2V").
+    success(function(data) {
+      if(data.Results.length === 0) {
+          $('.error').html("No result found. Please Enter Another Recipe Name");
+          $('.error').show();
+          $('.load').hide();
+      }
+      $('.load').hide();
+      $scope.Results = data;
+      $scope.recipes = data.Results;
+      console.log(data);
+      
+    });
+    $('.error').hide();
   };
 }]);
