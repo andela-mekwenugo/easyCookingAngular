6024d8660671f761bf28b424ba75dc82c3fd83c1
mirabelkoso <mirabelkoso@gmail.com>
2014-12-26 13:20:40 +0100

    added error handling

--- app/js/custom/app.js
+++ app/js/custom/app.js
@@ -3,22 +3,3 @@
   $('.loadArea').prepend('<p class="load">Searching...</p>');
 
   $('.load').hide();
-  
-  // notValid = function(error) {
-  //   if (error.status === 404) {
-  //     alert("oops!!!File not found");
-  //   } else if (error.status === 500) {
-  //     alert('enter another recipe please');
-  //   } else {
-  //     $scope.searchRecipe();
-  //   }
-  // }
-
-  // $('#searchButton').click(function(event){
-  //     event.preventDefault();
-  //     recipeName = parseInt($('#textArea').val());
-  //     notValid();
-
-  //   })
-
-
--- app/js/custom/controller.js
+++ app/js/custom/controller.js
@@ -9,10 +9,13 @@ recipeApp.controller('recipeController',['$scope', '$http', function($scope,$htt
       $('.load').show();
       $http.get("http://api.bigoven.com/recipes?title_kw=" + $scope.recipeName + "&pg=1&rpp=20&api_key=dvx0hom7nq400HGdFUJ164n6U7H9eA2V").
       success(function(data) {
-        $('.load').hide();
+        if(data.Results.length === 0) {
+            $('.loadArea').html("No result found. Please Enter Another Recipe Name");
+            $('.load').hide();
+        }
         $scope.Results = data;
         $scope.recipes = data.Results;
         console.log(data);
-    });
+      });
   };
 }]);
